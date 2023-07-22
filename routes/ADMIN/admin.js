b8a7fb88ab78c91b77dbe3b52a1d1e67056ddce8
EpaphrasSam <isinesam@gmail.com>
2023-07-22 04:26:28 +0000

    Fixed upload and delete

--- routes/ADMIN/admin.js
+++ routes/ADMIN/admin.js
@@ -1,126 +1,107 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../models/database");
-const courseRouter = require('./courses');
-const { Deta } = require('deta');
-const upload = require('express-fileupload');
-const fs = require('fs')
-require('dotenv').config();
-
+const courseRouter = require("./courses");
+const { Deta } = require("deta");
+const upload = require("express-fileupload");
+const fs = require("fs");
+require("dotenv").config();
 
 router.use("/course", courseRouter);
 router.use(upload());
-const deta = Deta(projectKey = process.env.DETA_KEY);
-const courseBooks = deta.Drive(driveName = process.env.DETA_DRIVENAME);
+const deta = Deta((projectKey = process.env.DETA_KEY));
+const courseBooks = deta.Drive((driveName = process.env.DETA_DRIVENAME));
 
 //Creates a form to allow user upload a book
 
-router.get('/', (req, res) => {
-    res.send(`
+router.get("/", (req, res) => {
+  res.send(`
     <form action="admin/upload" enctype="multipart/form-data" method="post">
       <input type="file" name="filetoUpload" required accept="application/pdf, .txt, .docx , .doc, .pptx"> 
       <input type="submit" value="Upload">
     </form>`);
-    //accept attribute restricts the upload form to only specified file types
-    console.log('Starting file upload');
+  //accept attribute restricts the upload form to only specified file types
+  console.log("Starting file upload");
 });
 
 //this request handles the upload of the specified book to the Deta drive
 router.post("/upload", async (request, response, next) => {
-    const { programme, level, semester, courseName } = request.body;
-    const directory = `${programme}/${level}/${semester}/${courseName}/Slides`
-    const names = `${directory}/${request.files.filetoUpload.name}`;
-    const contents = request.files.filetoUpload.data;
-    const storeBook = await courseBooks.put(names, { data: contents });
+  const { programme, level, semester, courseName } = request.body;
+  const directory = `${programme}/${level}/${semester}/${courseName}/Slides`;
+  const names = `${directory}/${request.files.filetoUpload.name}`;
+  const contents = request.files.filetoUpload.data;
+  await courseBooks.put(names, { data: contents });
 
-    const direct = `${programme}/${level}/${semester}/${courseName}/Slides/`
-    const array = []
-    const ext = []
-    const bookList = await courseBooks.list(
-        { prefix: direct });
-    const bookShelf = bookList.names;
-    const myarray = bookShelf.map(x => x.split(direct))
+  const direct = `${programme}/${level}/${semester}/${courseName}/Slides/`;
+  const bookList = await courseBooks.list({ prefix: direct });
+  const bookShelf = bookList.names;
 
-    var i = 0;
-    myarray.forEach(element => {
-        ext[0] = element.map(x => x.split(`.`))
-        array[i] = ext[0][1][0]
-        i = i + 1
-    });
+  const prog = programme.split(" ");
+  const progr = prog[0].toLowerCase();
 
-    const prog = programme.split(" ");
-    const progr = prog[0].toLowerCase();
+  const slideNames = bookShelf.map((name) => name.split(direct)[1]);
 
-    const Arrlength = array.length;
-    const newLectarr = [];
-    for (let i = 1; i <= Arrlength; i++) {
-        newLectarr.push(i);
-    }
-
-    const jsonArr = JSON.stringify(newLectarr);
-    const sql = `UPDATE CourseInfo SET slides = ?, ext=? WHERE IDM =? AND name=?`;
-    db.query(sql, [jsonArr, ext[0][1][1], progr, courseName])//Edit program ere
-    response.send(` ${progr} ${courseName} lectures uploaded to database: ` + jsonArr);
-
-    console.log('Successful file upload');
+  try {
+    const sql = `UPDATE CourseInfo SET slides = ? WHERE IDM = ? AND name = ?`;
+    db.query(sql, [JSON.stringify(slideNames), progr, courseName]);
+    response.send(
+      `${progr} ${courseName} lectures uploaded to database: ${slideNames}`
+    );
+    console.log("Successful file upload");
+  } catch (error) {
+    console.error("Error updating database:", error);
+    response.status(500).send("Error uploading lectures to database.");
+  }
 });
 
 router.post("/delete", async (request, response, next) => {
-    const { programme, level, semester, courseName, filetoDelete } = request.body;
-    const directory = `${programme}/${level}/${semester}/${courseName}/Slides/${filetoDelete}`
-    const deleteBook = await courseBooks.delete(directory);
+  const { programme, level, semester, courseName, filetoDelete } = request.body;
+  const directory = `${programme}/${level}/${semester}/${courseName}/Slides/${filetoDelete}`;
+  await courseBooks.delete(directory);
 
-    const direct = `${programme}/${level}/${semester}/${courseName}/Slides/`
-    const array = []
-    const ext = []
-    const bookList = await courseBooks.list(
-        { prefix: direct });
-    const bookShelf = bookList.names;
-    const myarray = bookShelf.map(x => x.split(direct))
+  const direct = `${programme}/${level}/${semester}/${courseName}/Slides/`;
+  const bookList = await courseBooks.list({ prefix: direct });
+  const bookShelf = bookList.names;
 
-    var i = 0;
-    myarray.forEach(element => {
-        ext[0] = element.map(x => x.split(`.`))
-        array[i] = ext[0][1][0]
-        i = i + 1
-    });
+  const prog = programme.split(" ");
+  const progr = prog[0].toLowerCase();
 
-    const prog = programme.split(" ");
-    const progr = prog[0].toLowerCase();
-    const exte = filetoDelete.slice(-3);
+  const slideNames = bookShelf.map((name) => name.split(direct)[1]);
 
-    const Arrlength = array.length;
-    const newLectarr = [];
-    for (let i = 1; i <= Arrlength; i++) {
-        newLectarr.push(i);
-    }
+  // Find the index of the filetoDelete in slideNames array and remove it
+  const indexToDelete = slideNames.indexOf(filetoDelete);
+  if (indexToDelete !== -1) {
+    slideNames.splice(indexToDelete, 1);
+  }
 
-    const jsonArr = JSON.stringify(newLectarr);
-    const sql = `UPDATE CourseInfo SET slides = ?, ext=? WHERE IDM =? AND name=?`;
-    { db.query(sql, [jsonArr, jsonArr === [] ? "" : exte, progr, courseName]) }
-    response.send(` ${progr} ${courseName} lectures uploaded to database: ` + jsonArr);
-
-    console.log('Successful Deletion');
+  try {
+    const sql = `UPDATE CourseInfo SET slides = ? WHERE IDM = ? AND name = ?`;
+    db.query(sql, [JSON.stringify(slideNames), progr, courseName]);
+    response.send(
+      `${progr} ${courseName} lectures uploaded to database: ${slideNames}`
+    );
+    console.log("Successful Deletion");
+  } catch (error) {
+    console.error("Error updating database:", error);
+    response.status(500).send("Error deleting lecture from database.");
+  }
 });
 
-router.get('/name', async (request, response,) => {
-    response.send(`<form action="admin/rename" enctype="multipart/form-data" method="post" >
+router.get("/name", async (request, response) => {
+  response.send(`<form action="admin/rename" enctype="multipart/form-data" method="post" >
     <label for="oldName">OldName:</label>
     <input type="text" name="oldName" id="oldName">
-    </form>`)
-    console.log('Starting file rename');
-
+    </form>`);
+  console.log("Starting file rename");
 });
 
-router.get('/rename', async (request, response,) => {
-    const { oldName } = req.body;
-    const { newName } = req.body;
-    fs.rename(`${oldName}`, `${newName}`)
-    let sql = `UPDATE Courses SET img = "https://th.bing.com/th/id/R.d03c6da728d2d9db994ced40346e68d5?rik=cMUcZXiBNEIdAg&pid=ImgRaw&r=0" WHERE IDM = "biomedical" AND name="Algebra" `;;
-    const result = await db.promise().query(sql, [newName])
-    console.log("File Renamed")
+router.get("/rename", async (request, response) => {
+  const { oldName } = req.body;
+  const { newName } = req.body;
+  fs.rename(`${oldName}`, `${newName}`);
+  let sql = `UPDATE Courses SET img = "https://th.bing.com/th/id/R.d03c6da728d2d9db994ced40346e68d5?rik=cMUcZXiBNEIdAg&pid=ImgRaw&r=0" WHERE IDM = "biomedical" AND name="Algebra" `;
+  const result = await db.promise().query(sql, [newName]);
+  console.log("File Renamed");
 });
-
 
 module.exports = router;
-
--- routes/course.js
+++ routes/course.js
@@ -1,37 +1,35 @@
-
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const db = require('../models/database');
-const {updateAccess} = require('../utils/helper')
-
+const db = require("../models/database");
+const { updateAccess } = require("../utils/helper");
 
 router.use((request, response, next) => {
-    console.log({ "COURSE": request.sessionID });
-    if (request.user) next()
-    else {
-        response.status(401).send({ msg: "User is not Logged In" })
-    }
-})
-
-router.get('/:programme', async (request, response,) => {
-    const { programme } = request.params;
-    console.log(programme);
-    let sql = `SELECT name,IDM,id,year,semester,img FROM  CourseInfo WHERE IDM = ?`;
-    const result = await db.promise().query(sql, [programme]);
-    response.send(result[0]);
+  console.log({ COURSE: request.sessionID });
+  if (request.user) next();
+  else {
+    response.status(401).send({ msg: "User is not Logged In" });
+  }
 });
 
-router.get('/:programme/:course', async (request, response) => {
-    const { programme, course } = request.params;
-    await updateAccess(programme, course)
-    const slides_books = [];
-    let sql = `SELECT * FROM  CourseInfo WHERE IDM = ? AND id=?`;
-    const slides = await db.promise().query(sql, [programme, course]);
-    slides_books[0] = slides[0];
-    sql = `SELECT * FROM CourseBooks WHERE courseName = ?`
-    const books = await db.promise().query(sql, [slides[0][0].name]);
-    slides_books[1] = books[0];
-    
-    response.send(slides_books);
-})
+router.get("/:programme", async (request, response) => {
+  const { programme } = request.params;
+  console.log(programme);
+  let sql = `SELECT name,IDM,id,year,semester,img FROM  CourseInfo WHERE IDM = ?`;
+  const result = await db.promise().query(sql, [programme]);
+  response.send(result[0]);
+});
+
+router.get("/:programme/:course", async (request, response) => {
+  const { programme, course } = request.params;
+  await updateAccess(programme, course);
+  const slides_books = [];
+  let sql = `SELECT * FROM  CourseInfo WHERE IDM = ? AND id=?`;
+  const slides = await db.promise().query(sql, [programme, course]);
+  slides_books[0] = slides[0];
+  sql = `SELECT * FROM CourseBooks WHERE courseName = ?`;
+  const books = await db.promise().query(sql, [slides[0][0].name]);
+  slides_books[1] = books[0];
+
+  response.send(slides_books);
+});
 module.exports = router;
