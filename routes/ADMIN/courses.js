dcb8bbf5b9d10c5530bd66dbc8d1252f67090a58
EpaphrasSam <isinesam@gmail.com>
2023-07-24 22:10:40 +0000

    Updates to admin routes

--- routes/ADMIN/courses.js
+++ routes/ADMIN/courses.js
@@ -1,22 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../../models/database')
+const db = require("../../models/database");
 
-router.get('/', async (request, response) => {
-    const sql = 'SELECT name FROM CourseInfo';
-    const results = await db.promise().query(sql);
-    const courses = results[0];
-    response.status(200).send(courses);
+router.get("/", async (request, response) => {
+  const sql = "SELECT * FROM CourseInfo";
+  const results = await db.promise().query(sql);
+  const courses = results[0];
+  response.status(200).send(courses);
+});
 
-
-})
-
-router.get('/users', async (request, response) => {
-    const sql = 'SELECT id FROM student';
-    const results = await db.promise().query(sql);
-    const users = results[0];
-    response.status(200).send(users);
-
-
-})
+router.get("/users", async (request, response) => {
+  const sql = "SELECT id FROM student";
+  const results = await db.promise().query(sql);
+  const users = results[0];
+  response.status(200).send(users);
+});
 module.exports = router;
